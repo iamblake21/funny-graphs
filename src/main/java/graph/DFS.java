6d3677cb31e2ba38428c457d92c6e33e96f33799
Salvatore Liccardo <s.liccardo022@gmail.com>
2026-02-14 18:33:19 +0100

    Enhance DFS and BFS documentation with detailed explanations and examples

--- src/main/java/graph/DFS.java
+++ src/main/java/graph/DFS.java
@@ -21,15 +21,70 @@ public class DFS {
 
 
     // Ti faccio notare che letterlamente bfs e dfs sono identici, 
-    // cambia solo la struttura dati che usi per tenere traccia dei nodi da visitare, 
-    // e il fatto che in dfs devi tenere traccia dei nodi già visitati,
-    // altrimenti rischi di entrare in un ciclo infinito.
+    // cambia solo la struttura dati che usi.
 
+
+    // BFS:
+    // Parto da A, segno A come visitato, e lo metto in coda per scoprirne i vicini.
+    // Coda: [A]
+    // A, visitato.
+    // Finchè la coda non è vuota prendo il primo elemento e ne scopro i vicini.
+    // Coda: []
+    // Scopro i vicini di A, che sono B e C, e li metto in coda per scoprirne i vicini, se non li ho già visitati.
+    // Coda: [B, C]
+    // Prendo B, lo segno come visitato, e scopro i suoi vicini, che sono A, ma è già stato visitato, quindi non lo metto in coda.
+    // Coda: [C]
+    // Prendo C, lo segno come visitato, e scopro i suoi vicini, che sono A e D, ma A è già stato visitato, quindi non lo metto in coda, mentre D è nuovo, quindi lo metto in coda.
+    // Coda: [D]
+    // Prendo D, lo segno come visitato, e scopro i suoi vicini, che sono C, ma è già stato visitato, quindi non lo metto in coda.
+    // Coda: []
+    // Finito.
+    // Praticamente, in coda ci sono sempre i nodi che ho scoperto ma di cui non ho ancora scoperto i vicini, 
+    // e ogni volta che prendo un nodo dalla coda, lo segno come visitato, e scopro i suoi vicini, che se non li ho già visitati, li metto in coda per scoprirne i vicini dopo.
+
+    // DFS:
+    // Parto da A, segno A come visitato, e scopro i suoi vicini, che sono B e C, e li visito uno alla volta, se non li ho già visitati.
+    // A, visitato.
+    // Scopro i vicini di A, che sono B e C, e li visito uno alla volta, se non li ho già visitati.
+    // Visito B, lo segno come visitato, e scopro i suoi vicini, che sono A, ma è già stato visitato, quindi non lo visito.
+    // B, visitato.
+    // Visito C, lo segno come visitato, e scopro i suoi vicini, che sono A e D, ma A è già stato visitato, quindi non lo visito, mentre D è nuovo, quindi lo visito.
+    // C, visitato.
+    // Visito D, lo segno come visitato, e scopro i suoi vicini, che sono C, ma è già stato visitato, quindi non lo visito.
+    // D, visitato.
+    // Finito.
+    // Praticamente, in dfs, ogni volta che visiti un nodo, 
+    // lo segni come visitato, e scopro i suoi vicini, che se non li hai già visitati,
+    //  li visiti uno alla volta, e per ognuno di essi ripeti lo stesso processo,
+    //  fino a quando non hai visitato tutti i nodi raggiungibili dal nodo di partenza.
+    // Grafo
+    //     A
+    //    / \
+    //   B   C
+    //   |    |
+    //    D   E
+
+
+
+    // Un altra differenza sostanziale è il momento in cui segni come "visitato" un nodo.
+    // Nella dfs, segni come visitato un nodo quando nel momento in cui lo visiti, mentre nella bfs, segni come visitato un nodo quando lo metti in coda per scoprirne i vicini dopo.
+    // Cosa voglio dire:
+    // Nella dfs, un nodo è visitato quando stai scoprendo i suoi vicini...
+    // In questa bfs invece, un nodo è visitato quando viene scoperto come vicino di un nodo che stai visitando, e lo metti in coda per scoprirne i vicini dopo, così da evitare di metterlo più volte in coda, e quindi di visitarlo più volte, e rischiare di entrare in un ciclo infinito.
+    // Ovviamente, non è detto che una bfs non possa essere implementata segnando come visitato un nodo quando lo visiti, ma c'è un rischio, che è quello di mettere più volte in coda lo stesso nodo, 
+    // e quindi di visitarlo più volte, questo non causa loop infinito, ma sicuramente è inefficiente, perchè stai visitando più volte lo stesso nodo,
+    //  e quindi stai facendo più lavoro del necessario.
+    
+    
+    
     public void bfs (int seed, Graph g) {
         Queue<Integer>  queue= new ArrayDeque<>(); // Inizializzo la coda
         Set<Integer> visited = new HashSet<>(); // Inizializzo il set
         queue.add(seed); // Aggiungo il seed alla coda, e lo segno come già visitato
-        visited.add(seed);
+        visited.add(seed); // Perchè se non lo segno come già visitato, quando lo prendo dalla coda,
+        //  e scopro i suoi vicini, li metto in coda, e quando li prendo dalla coda, non so se li ho già visitati o no, 
+        // e rischio di visitarli più volte, rendendo l'algoritmo inefficiente.
+
 
         while (!queue.isEmpty()) { //Finchè non esaurisco la coda
             int current = queue.poll(); //Prendo il primo elemento.
@@ -44,6 +99,21 @@ public class DFS {
 
 
     }
+
+    // Lo stack è una struttura dati che funziona come una pila,
+    // ovvero, l'ultimo elemento inserito è il primo ad essere rimosso (LIFO - Last In, First Out).
+    
+    // La coda, invece, è una struttura dati che funziona come una fila,
+    // ovvero, il primo elemento inserito è il primo ad essere rimosso (FIFO - First In, First Out)
+
+
+        // Grafo
+    //     A
+    //    / \
+    //   B   C
+    //   |   |
+    //   D   E
+
     
 
     public void iterativeDfs (int seed, Graph g) {
